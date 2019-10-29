129f3ed152fb42115b8926e54635f52ccf312981
samreetira123 <57062614+samreetira123@users.noreply.github.com>
2019-10-29 16:51:21 +0700

    ็added homework

---
+++ Exercise4_Samree_T.py
@@ -0,0 +1,11 @@
+foun_Eng = 70
+gen_Busi = 65.25
+int_Comp =55.75
+comp_Pro = 82.50
+print("----------------- Your Score -------------")
+print("Foundation English :",foun_Eng)
+print("General Business :",gen_Busi)
+print("Introduction to Computer Systems :",int_Comp)
+print("Computer Programming :",comp_Pro)
+
+
