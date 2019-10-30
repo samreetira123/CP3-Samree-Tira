abb2e698ff1248204c534f4dc3df6a385eccf105
samreetira123 <57062614+samreetira123@users.noreply.github.com>
2019-10-30 16:25:24 +0700

    Exercise5

---
+++ Exercise5_1_Samree_T.py
@@ -0,0 +1,12 @@
+Num1 = 100
+Num2 = 5
+
+Ans1 = Num1 + Num2
+Ans2 = Num1 - Num2
+Ans3 = Num1 * Num2
+Ans4 = Num1 / Num2
+
+print(Num1, "+", Num2, "=", Ans1)
+print(Num1, "-", Num2, "=", Ans2)
+print(Num1, "*", Num2, "=", Ans3)
+print(Num1, "/", Num2, "=", Ans4)
---
+++ Exercise5_2_Samree_T.py
@@ -0,0 +1,5 @@
+v = float(input("distance[km] : "))
+t = float(input("time[hr] : "))
+
+print(int(v / t),"km/h")
+
