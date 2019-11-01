9f8c4254f56e51e89bce7dde4f4d21c3b7362327
samreetira123 <57062614+samreetira123@users.noreply.github.com>
2019-11-01 17:45:25 +0700

    Update Lecture53_Samree_T.py
    update

--- Lecture53_Samree_T.py
+++ Lecture53_Samree_T.py
@@ -2,5 +2,4 @@ def vatCalculator(totalPrice):
     result = totalPrice+(totalPrice * 7 /100)
     return  result
 
-totalPrice = int(input("Total Price : "))
-print(vatCalculator(totalPrice))
+print(vatCalculator(int(input("Total Price : "))))
