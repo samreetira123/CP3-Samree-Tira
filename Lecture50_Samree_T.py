7dd657dd7dcb241b1c295d9844bdc5b834089671
samreetira123 <57062614+samreetira123@users.noreply.github.com>
2019-11-01 16:24:22 +0700

    added Lecture50

---
+++ Lecture50_Samree_T.py
@@ -0,0 +1,13 @@
+def addNumber (a,b):
+    print(a+b)
+def subNumbers(c,d):
+    print(c-d)
+def multiNumbers(e,f):
+    print(e*f)
+def divNumbers(g,h):
+    print (g/h)
+
+addNumber(5,5)
+subNumbers(10,5)
+multiNumbers(2,10)
+divNumbers(30,30)
