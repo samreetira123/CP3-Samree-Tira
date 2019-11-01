cca3f3f30fdc94850dc941ac0c13bba08e504ed3
samreetira123 <57062614+samreetira123@users.noreply.github.com>
2019-11-01 15:47:53 +0700

    Added Exercise11

--- Exercise11_Samree_T.py
+++
@@ -1,5 +0,0 @@
-userinput = int(input())
-sum = 0
-for x in range(userinput):
-        print(" "*(userinput-x)+"*"*(sum+1))
-        sum = sum + 2
---
+++ Exercise_11_Samree_T.py
@@ -0,0 +1,5 @@
+Input = int(input())
+Intext = 0
+for i in range(Input) :
+    print(" "*(Input+Intext)+"*"*((2*(i+1))-1 ))
+    Intext -= 1
